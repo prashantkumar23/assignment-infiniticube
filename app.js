b1ef8131c1ee982cd5e8746bb16d3d90948c642a
Prashant Kumar <prashantdrew23@gmail.com>
2022-01-05 21:26:10 +0530

    code upload

---
+++ app.js
@@ -0,0 +1,114 @@
+const express = require("express");
+const { Client } = require("pg");
+
+const app = express();
+
+const client = new Client({
+  host: "rogue.db.elephantsql.com",
+  user: "mjswfhiv",
+  database: "mjswfhiv",
+  password: "KKpxxrYJNbqMYtDj7i4YiCWJWthUzp08",
+});
+
+app.get("/orders", (req, res) => {
+  let numRows;
+  let numPerPage = parseInt(req.query.limit, 10) || 1;
+  let page = parseInt(req.query.page, 10) || 0;
+  let numPages;
+  let skip = page * numPerPage;
+  let limit = skip + numPerPage;
+  const { user_id } = req.query;
+
+  client
+    .query("select count(*) from orders")
+    .then((results) => {
+      numRows = results.rows[0].count;
+      numPages = Math.floor(numRows / numPerPage);
+      console.log("number of pages:", numPages);
+    })
+    .then(() =>
+      client.query(
+        `SELECT * from orders where user_id=${user_id} limit ${limit}`
+      )
+    )
+    .then((results) => {
+      const responsePayload = {
+        results,
+      };
+
+      if (page < numPages) {
+        responsePayload.pagination = {
+          current: page,
+          perPage: numPerPage,
+          previous: page > 1 ? page - 1 : undefined,
+          next: page < numPages - 1 ? page + 1 : undefined,
+        };
+      } else
+        responsePayload.pagination = {
+          err:
+            "queried page " +
+            page +
+            " is >= to maximum page number " +
+            numPages,
+        };
+      res.json({
+        Total: numRows,
+        Data: responsePayload.results.rows,
+      });
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.json({ err: err });
+    });
+});
+
+app.get("/users", (req, res) => {
+  let numRows;
+  let numPerPage = parseInt(req.query.limit, 10) || 1;
+  let page = parseInt(req.query.page, 10) || 1;
+  let numPages;
+  let skip = page * numPerPage;
+  let limit = skip + numPerPage;
+
+  client
+    .query(
+      `select * from users left join orders on users.id = orders.user_id limit ${limit}`
+    )
+    .then((results) => {
+      numRows = results.rowCount;
+      numPages = Math.floor(numRows / numPerPage);
+      console.log("number of pages:", numPages);
+      const responsePayload = {
+        results,
+      };
+
+      if (page < numPages) {
+        responsePayload.pagination = {
+          current: page,
+          perPage: numPerPage,
+          previous: page > 1 ? page - 1 : undefined,
+          next: page < numPages - 1 ? page + 1 : undefined,
+        };
+      } else
+        responsePayload.pagination = {
+          err:
+            "queried page " +
+            page +
+            " is >= to maximum page number " +
+            numPages,
+        };
+      res.json({
+        Total: numRows,
+        Data: responsePayload.results.rows,
+      });
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.json({ err: err });
+    });
+});
+
+app.listen(4000, async () => {
+  await client.connect().then(() => console.log("Database Connected"));
+  console.log("Server running ");
+});
